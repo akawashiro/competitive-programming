5952f7f2bd0bd3c5eac04b81893c28f80cd4150c
Akira KAWATA <akirakawata1@gmail.com>
2017-05-01 12:13:10 +0900

    Google Code Jam 2017 Round1C

---
+++ GoogleCodeJam2017Round1C/4/check.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+g++ -std=c++11 source.cpp -Wall -o execution.out
+if [ $? -eq 1 ]
+then
+    exit 1
+fi
+
+printf "\n"
+
+i=1
+while [ -e input$i.txt ]
+do
+    if [ ! -s input$i.txt ] 
+    then
+        i=`expr $i + 1`
+        continue
+    fi
+    ./execution.out < input$i.txt > output$i.txt
+    printf "============================================\n"
+    printf "check-$i\n"
+    l=`cat output$i.txt | wc -l `
+    #c=`wc output$i.txt  -L | awk '{print $1}' `
+    #if [ $l -lt 100 ] && [ $c -lt 1000 ]
+    if [ $l -lt 100 ]
+    then
+        cat output$i.txt
+    else
+        printf "too long"
+    fi
+    printf "\n"
+    i=`expr $i + 1`
+done
+printf "\n"
+
