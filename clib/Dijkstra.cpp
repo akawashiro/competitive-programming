893b8e7f597fb7c8d1e650137bce1d9a3e278f2f
akira <a.kawashiro@gmail.com>
2017-12-23 16:37:08 +0900

    Auto commit.

--- clib/Dijkstra.cpp
+++ clib/Dijkstra.cpp
@@ -15,14 +15,14 @@ int dijkstra(int s,int g)
 {
     vector<int> dist(n,INT_MAX/2);
     priority_queue<Edge> que;
-    que.push(Edge(s,0));
+    que.push(Edge{s,0});
     while(!que.empty()){
         Edge e=que.top();
         que.pop();
         if(e.cost<dist[e.to]){
             dist[e.to]=e.cost;
             for(int i=0;i<(int)G[e.to].size();i++)
-                que.push(Edge(G[e.to][i].to,e.cost+G[e.to][i].cost));
+                que.push(Edge{G[e.to][i].to,e.cost+G[e.to][i].cost});
         }
     }
     return dist[g];
