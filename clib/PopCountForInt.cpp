af67bbc911a42888f40ca3b0b78a075a3a74d189
akira <a.kawashiro@gmail.com>
2017-12-25 00:35:12 +0900

    Auto commit.

--- clib/PopCountForInt.cpp
+++
@@ -1,9 +0,0 @@
-int popCount(int _mask)
-{
-	int r=0;
-	for(int i=0;i<31;i++)
-		if(_mask & (1<<i))
-			r++;
-	return r;
-}
-
--- clib/PopCountForLL.cpp
+++
@@ -1,9 +0,0 @@
-int popCount(LL _mask)
-{
-	int r=0;
-	for(LL i=0;i<62;i++)
-		if(_mask & (LL(1)<<i))
-			r++;
-	return r;
-}
-
