5c4c5efdfd1780dbb6f0bcd75872f977a42bacc7
Akira KAWATA <akirakawata1@gmail.com>
2018-05-30 10:43:27 +0900

    Modify prime number library.

--- clib/Eratosthenes.cpp
+++ clib/Eratosthenes.cpp
@@ -1,7 +1,12 @@
-#define MAX_N
-for(int i=0;i<MAX_N;i++)	prime[i]=1;
-prime[0]=prime[1]=0;
-	for(int i=0;i<MAX_N;i++)
-if(prime[i])
-	for(int j=i+i;j<MAX_N;j+=i)
-	prime[j]=0;
+#define MAX_N 10000
+int prime[MAX_N];
+
+void eras(){
+    for(int i=0;i<MAX_N;i++)
+        prime[i]=1;
+    prime[0]=prime[1]=0;
+    for(int i=0;i<MAX_N;i++)
+        if(prime[i])
+            for(int j=i+i;j<MAX_N;j+=i)
+                prime[j]=0;
+}
